3b4521123f013d38957869b61d79b487fc362ae4
thanhKasper <kieutienthanh207@gmail.com>
2024-05-04 10:58:29 +0700

    fix: fix import error

--- components/Filter.jsx
+++ components/Filter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FilterDispatch } from '@/context/Context';
 import { useContext } from 'react';
 
